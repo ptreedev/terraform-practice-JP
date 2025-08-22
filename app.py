e6784e4e280aa08a22aedfce114699db18e30229
Peter Leaver <peteleaver@madetech.com>
2025-08-22 13:11:22 +0100

    ADD

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "<p> Hello World </p>"
+    return "<p> Hello World! </p>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
